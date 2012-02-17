3ff1f363f6838c6280722f3df5e86a67f3f7c506
Luca Pradovera <luca.pradovera@gmail.com>
2012-02-17 13:11:02 +0100

    Getting started

---
+++ lib/plugin-template.rb
@@ -0,0 +1,7 @@
+require "plugin-template/version"
+
+module Plugin
+  module Template
+    # Your code goes here...
+  end
+end
---
+++ lib/plugin-template/version.rb
@@ -0,0 +1,5 @@
+module Plugin
+  module Template
+    VERSION = "0.0.1"
+  end
+end
